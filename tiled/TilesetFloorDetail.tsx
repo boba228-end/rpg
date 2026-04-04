8b3aa127348c1789a5998e70327d523feac8e726
gitmskhl <gglmskhl@gmail.com>
2026-04-04 13:09:31 +0300

    spiritworld

---
+++ tiled/TilesetFloorDetail.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1-8-g449813058" name="TilesetFloorDetail" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="../../../../../../../Загрузки/Ninja Adventure - Asset Pack/Backgrounds/Tilesets/TilesetFloorDetail.png" width="256" height="80"/>
+</tileset>
---
+++ tiled/TilesetTowers.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1-8-g449813058" name="TilesetTowers" tilewidth="32" tileheight="32" tilecount="36" columns="12">
+ <image source="../../../../../../../Загрузки/Ninja Adventure - Asset Pack/Backgrounds/Tilesets/TilesetTowers.png" width="384" height="96"/>
+</tileset>
