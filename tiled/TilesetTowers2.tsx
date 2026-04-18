ad01e3475d58392bb7f55fa2cf0d6fb0a03d8977
gitmskhl <gglmskhl@gmail.com>
2026-04-18 13:02:45 +0300

    new dialog

---
+++ tiled/TilesetTowers2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1-8-g449813058" name="TilesetTowers" tilewidth="16" tileheight="16" tilecount="144" columns="24">
+ <image source="../../../../../../../Загрузки/Ninja Adventure - Asset Pack/Backgrounds/Tilesets/TilesetTowers.png" width="384" height="96"/>
+</tileset>
